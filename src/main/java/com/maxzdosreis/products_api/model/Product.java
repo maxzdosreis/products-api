258b9b542da6b43079d917cdd05806a685abd0fa
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2025-11-06 01:12:38 -0300

    feat(model): adiciona o entidade de produtos
    Adicionada algumas validações na entidade

---
+++ src/main/java/com/maxzdosreis/products_api/model/Product.java
@@ -0,0 +1,38 @@
+package com.maxzdosreis.products_api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, length = 250)
+    private String name;
+
+    @NotNull
+    @Positive
+    private Double price;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    @Positive
+    private Integer quantity;
+}
