4fe201eb599608ec2ac255630cab421a38869356
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2025-11-08 22:13:58 -0300

    fix(controller): altera requestMapping do controller
    realizado melhoria para adicionar '/api' na frente dos endereços dos controllers

--- src/main/java/com/maxzdosreis/products_api/controller/ProductController.java
+++ src/main/java/com/maxzdosreis/products_api/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/products")
+@RequestMapping("/api/products")
 @RestController
 public class ProductController {
 
