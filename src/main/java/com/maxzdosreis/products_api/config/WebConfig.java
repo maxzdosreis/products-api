36437953b7fb6f50dfb2974640c950ee87af9a66
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2025-11-08 22:33:04 -0300

    feat(config): adicionado Cors e Content Negotiation
    - adicionado content negotiation para possíveis implementações futuras, para produzir e consumir outras extensões
    
    - desabilitada a dependência do Spring Security, pois será implementada daqui alguns commits

---
+++ src/main/java/com/maxzdosreis/products_api/config/WebConfig.java
@@ -0,0 +1,35 @@
+package com.maxzdosreis.products_api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                    .mediaType("json", MediaType.APPLICATION_JSON)
+                    // adicionado as extensões xml e yaml, para no futuro produzir ou consumir outras exntensões além de json
+                    .mediaType("xml", MediaType.APPLICATION_XML)
+                    .mediaType("yaml", MediaType.APPLICATION_YAML);
+    }
+}
