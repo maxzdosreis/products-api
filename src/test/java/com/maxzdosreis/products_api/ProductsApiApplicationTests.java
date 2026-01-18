9a221fe82d464423e698ce9bf2fb7853ffccc790
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2026-01-17 22:45:02 -0300

    fix(test): corrige os testes para build ser aprovada

--- src/test/java/com/maxzdosreis/products_api/ProductsApiApplicationTests.java
+++ src/test/java/com/maxzdosreis/products_api/ProductsApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.maxzdosreis.products_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductsApiApplicationTests {
 
 	@Test
