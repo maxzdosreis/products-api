a224d0553eef9de2b8b3df0074c3ccba0fef6947
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2026-04-11 00:21:50 -0300

    feat: adiciona métodos de Enable e Disable no Repository e no Service
    - Altera método delete, para que agora exclua realmente

--- src/main/java/com/maxzdosreis/products_api/repository/UserRepository.java
+++ src/main/java/com/maxzdosreis/products_api/repository/UserRepository.java
@@ -2,12 +2,21 @@ package com.maxzdosreis.products_api.repository;
 
 import com.maxzdosreis.products_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE User u SET u.enabled = false WHERE u.id =:id")
+    void disableUser(@Param("id") Long id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE User u SET u.enabled = true WHERE u.id =:id")
+    void enableUser(@Param("id") Long id);
 
     @Query("SELECT u FROM User u WHERE u.userName = :userName")
     User findByUsername(@Param("userName") String username);
